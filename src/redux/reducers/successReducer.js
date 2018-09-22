a15ef661bd1b17156fa045b76b616c2cde21d13d
pavelusov <ipavelusov@gmail.com>
2018-09-22 19:31:58 +0500

    Set up redux

---
+++ src/redux/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer - reducer for success segment
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action
+ * @return {bool} - new success state
+ */
+export default (state, action) => {
+  return null;
+}
